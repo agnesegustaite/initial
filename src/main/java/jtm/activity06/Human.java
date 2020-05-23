67643602bd754973a88e8f05d584a8c59ac179b9
Agnese <agnesegustaite@gmail.com>
2020-05-23 14:00:38 +0300

    Push to master

---
+++ src/main/java/jtm/activity06/Human.java
@@ -0,0 +1,35 @@
+package jtm.activity06;
+
+public class Human implements Humanoid {
+
+	@Override
+	public void eat(Integer food) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object vomit() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String isAlive() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String killHimself() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getWeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/main/java/jtm/activity06/Martian.java
@@ -0,0 +1,41 @@
+package jtm.activity06;
+
+public class Martian implements Humanoid, Alien, Cloneable {
+
+	@Override
+	public void eat(Object item) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void eat(Integer food) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object vomit() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String isAlive() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String killHimself() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getWeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
